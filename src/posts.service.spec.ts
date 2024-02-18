dfad2900987d2569f0d15b8c26b6d84eed20f0fe
Andrei <Abdrei19972910@yandex.by>
2024-02-19 02:00:52 +0300

    unittest1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,80 +1,33 @@
-<<<<<<< HEAD
 import { Post, PostsService } from "./posts.service";
+
 describe("PostsService", () => {
   let postsService: PostsService;
   const post: Omit<Post, "id" | "date"> = {
     text: "Mocked post",
-=======
-import { Post, PostsService } from './posts.service';
-
-describe('PostsService', () => {
-  let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
->>>>>>> 5878feff308fa6c70ee3d7185e5e43604b31b2b4
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-<<<<<<< HEAD
     postsService.create({ text: "Some pre-existing post" });
   });
 
   it("should add a new post", () => {
-    // Создаем новый пост
-    const newPost = {
-      text: "New test post",
-    };
-
-    // Добавляем пост с помощью метода create
-    const createdPost = postsService.create(newPost);
-
-    // Проверяем, что созданный пост определен
-    expect(createdPost).toBeDefined();
-
-    // Проверяем, что у поста есть id и дата
-    expect(createdPost.id).toBeDefined();
-    expect(createdPost.date).toBeDefined();
-
-    // Проверяем, что текст поста соответствует ожидаемому тексту
-    expect(createdPost.text).toEqual(newPost.text);
-
-    // Проверяем, что пост был добавлен в массив постов
-    const foundPost = postsService.find(createdPost.id);
-    expect(foundPost).toEqual(createdPost);
+    const created = postsService.create(post);
+    const createdPost = postsService.find(created.id);
+    expect(createdPost).toEqual({
+      id: created.id,
+      text: created.text,
+      date: created.date,
+    });
   });
 
   it("should find a post", () => {
-    // Создаем новый пост
-    const newPost = {
-      text: "Test post",
-    };
-    const createdPost = postsService.create(newPost);
-
-    const foundPost = postsService.find(createdPost.id);
-
-    expect(foundPost).toBeDefined();
-
-    if (foundPost) {
-      expect(foundPost.id).toEqual(createdPost.id);
-      expect(foundPost.text).toEqual(createdPost.text);
-      expect(foundPost.date).toEqual(createdPost.date);
-    } else {
-      throw new Error("Post not found");
-    }
+    const foundPost = postsService.find("1");
+    expect(foundPost).toEqual({
+      id: "1",
+      text: "Some pre-existing post",
+      date: expect.any(String),
+    });
   });
 });
-=======
-    postsService.create({ text: 'Some pre-existing post' });
-  });
-
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
-  });
-
-  it('should find a post', () => {
-    // реализуйте тест-кейс
-  });
-});
->>>>>>> 5878feff308fa6c70ee3d7185e5e43604b31b2b4
